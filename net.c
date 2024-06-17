13fdf6505884e3d2174ffd781e501df0a6e8ca62
YAMAMOTO Masaya <pandax381@gmail.com>
2024-06-18 08:02:39 +0900

    step15 開始

--- net.c
+++ net.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <string.h>
 
 #include "platform.h"
 
@@ -14,6 +15,13 @@ struct net_protocol {
     struct net_protocol *next;
     uint16_t type;
     net_protocol_handler_t handler;
+};
+
+struct net_protocol_queue_entry {
+    struct queue_entry entry;
+    struct net_device *dev;
+    size_t len;
+    /* data bytes exists after this structure. */
 };
 
 /*
@@ -174,6 +182,16 @@ net_protocol_register(uint16_t type, net_protocol_handler_t handler)
 	return 0;
 }
 
+static struct net_protocol_queue_entry *
+net_protocol_queue_push(struct net_protocol *proto, const uint8_t *data, size_t len, struct net_device *dev)
+{
+}
+
+static struct net_protocol_queue_entry *
+net_protocol_queue_pop(struct net_protocol *proto)
+{
+}
+
 int
 net_input(uint16_t type, const uint8_t *data, size_t len, struct net_device *dev)
 {
@@ -191,6 +209,18 @@ net_input(uint16_t type, const uint8_t *data, size_t len, struct net_device *dev
 	return 0;
 }
 
+<<<<<<< HEAD
+=======
+void
+net_softirq_handler(unsigned int irq, void *arg)
+{
+}
+
+#include "arp.h"
+#include "ip.h"
+#include "icmp.h"
+
+>>>>>>> 133801e (Skeleton of step15)
 int
 net_init(void)
 {
