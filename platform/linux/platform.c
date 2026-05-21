84119392a61d8c125c0501c061d6efc18169bc08
kazu260111 <254258422+kazu260111@users.noreply.github.com>
2026-05-21 19:27:21 +0900

    Completion of step11

--- platform/linux/platform.c
+++ platform/linux/platform.c
@@ -6,25 +6,35 @@
 
 #include "platform.h"
 
+#include "intr.h"
 #include "util.h"
 
 int
 platform_init(void)
 {
-    srandom(time(NULL));
-    return 0;
+	srandom(time(NULL));
+	if (intr_init() == -1) {
+		return -1;
+	}
+	return 0;
 }
 
 int
 platform_run(void)
 {
-    return 0;
+	if (intr_run() == -1) {
+		return -1;
+	}
+       	return 0;
 }
 
 int
 platform_shutdown(void)
 {
-    return 0;
+	if (intr_shutdown() == -1) {
+		return -1;
+	}
+	return 0;
 }
 
 /*
